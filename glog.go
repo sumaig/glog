4a2f4665d26ec447a56003abd5f0167c28b1a2d4
Sumai <jack.gao.ch@gmail.com>
2017-11-28 10:31:08 +0800

    code style

--- glog.go
+++ glog.go
@@ -1,34 +1,3 @@
-// Copyright 2017 sumai Author. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Package glog provide a general log interface
-// Usage:
-//
-// import "github.com/sumaig/glog"
-//
-//	log := NewLogger(10000)
-//	log.SetLogger("console", "")
-//
-//	> the first params stand for how many channel
-//
-// Use it like this:
-//
-//	log.Trace("trace")
-//	log.Info("info")
-//	log.Warn("warning")
-//	log.Debug("debug")
-//	log.Critical("critical")
 package glog
 
 import (
@@ -134,45 +103,45 @@ var logMsgPool *sync.Pool
 // channelLen means the number of messages in chan(used where asynchronous is true).
 // if the buffering chan is full, logger adapters write to file or other way.
 func NewLogger(channelLens ...int64) *GoLogger {
-	bl := new(GoLogger)
-	bl.level = LevelDebug
-	bl.loggerFuncCallDepth = 2
-	bl.msgChanLen = append(channelLens, 0)[0]
-	if bl.msgChanLen <= 0 {
-		bl.msgChanLen = defaultAsyncMsgLen
+	g := new(GoLogger)
+	g.level = LevelDebug
+	g.loggerFuncCallDepth = 2
+	g.msgChanLen = append(channelLens, 0)[0]
+	if g.msgChanLen <= 0 {
+		g.msgChanLen = defaultAsyncMsgLen
 	}
-	bl.signalChan = make(chan string, 1)
-	bl.setLogger(AdapterConsole)
-	return bl
+	g.signalChan = make(chan string, 1)
+	g.setLogger(AdapterConsole)
+	return g
 }
 
 // Async set the log to asynchronous and start the goroutine
-func (bl *GoLogger) Async(msgLen ...int64) *GoLogger {
-	bl.lock.Lock()
-	defer bl.lock.Unlock()
-	if bl.asynchronous {
-		return bl
+func (g *GoLogger) Async(msgLen ...int64) *GoLogger {
+	g.lock.Lock()
+	defer g.lock.Unlock()
+	if g.asynchronous {
+		return g
 	}
-	bl.asynchronous = true
+	g.asynchronous = true
 	if len(msgLen) > 0 && msgLen[0] > 0 {
-		bl.msgChanLen = msgLen[0]
+		g.msgChanLen = msgLen[0]
 	}
-	bl.msgChan = make(chan *logMsg, bl.msgChanLen)
+	g.msgChan = make(chan *logMsg, g.msgChanLen)
 	logMsgPool = &sync.Pool{
 		New: func() interface{} {
 			return &logMsg{}
 		},
 	}
-	bl.wg.Add(1)
-	go bl.startLogger()
-	return bl
+	g.wg.Add(1)
+	go g.startLogger()
+	return g
 }
 
 // SetLogger provides a given logger adapter into GoLogger with config string.
 // config need to be correct JSON as string: {"interval":360}.
-func (bl *GoLogger) setLogger(adapterName string, configs ...string) error {
+func (g *GoLogger) setLogger(adapterName string, configs ...string) error {
 	config := append(configs, "{}")[0]
-	for _, l := range bl.outputs {
+	for _, l := range g.outputs {
 		if l.name == adapterName {
 			return fmt.Errorf("logs: duplicate adaptername %q (you have set this logger before)", adapterName)
 		}
@@ -189,43 +158,43 @@ func (bl *GoLogger) setLogger(adapterName string, configs ...string) error {
 		fmt.Fprintln(os.Stderr, "logs.GoLogger.SetLogger: "+err.Error())
 		return err
 	}
-	bl.outputs = append(bl.outputs, &nameLogger{name: adapterName, Logger: lg})
+	g.outputs = append(g.outputs, &nameLogger{name: adapterName, Logger: lg})
 	return nil
 }
 
 // SetLogger provides a given logger adapter into GoLogger with config string.
 // config need to be correct JSON as string: {"interval":360}.
-func (bl *GoLogger) SetLogger(adapterName string, configs ...string) error {
-	bl.lock.Lock()
-	defer bl.lock.Unlock()
-	if !bl.init {
-		bl.outputs = []*nameLogger{}
-		bl.init = true
+func (g *GoLogger) SetLogger(adapterName string, configs ...string) error {
+	g.lock.Lock()
+	defer g.lock.Unlock()
+	if !g.init {
+		g.outputs = []*nameLogger{}
+		g.init = true
 	}
-	return bl.setLogger(adapterName, configs...)
+	return g.setLogger(adapterName, configs...)
 }
 
 // DelLogger remove a logger adapter in GoLogger.
-func (bl *GoLogger) DelLogger(adapterName string) error {
-	bl.lock.Lock()
-	defer bl.lock.Unlock()
+func (g *GoLogger) DelLogger(adapterName string) error {
+	g.lock.Lock()
+	defer g.lock.Unlock()
 	outputs := []*nameLogger{}
-	for _, lg := range bl.outputs {
+	for _, lg := range g.outputs {
 		if lg.name == adapterName {
 			lg.Destroy()
 		} else {
 			outputs = append(outputs, lg)
 		}
 	}
-	if len(outputs) == len(bl.outputs) {
+	if len(outputs) == len(g.outputs) {
 		return fmt.Errorf("logs: unknown adaptername %q (forgotten Register?)", adapterName)
 	}
-	bl.outputs = outputs
+	g.outputs = outputs
 	return nil
 }
 
-func (bl *GoLogger) writeToLoggers(when time.Time, msg string, level int) {
-	for _, l := range bl.outputs {
+func (g *GoLogger) writeToLoggers(when time.Time, msg string, level int) {
+	for _, l := range g.outputs {
 		err := l.WriteMsg(when, msg, level)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "unable to WriteMsg to adapter:%v,error:%v\n", l.name, err)
@@ -233,7 +202,7 @@ func (bl *GoLogger) writeToLoggers(when time.Time, msg string, level int) {
 	}
 }
 
-func (bl *GoLogger) Write(p []byte) (n int, err error) {
+func (g *GoLogger) Write(p []byte) (n int, err error) {
 	if len(p) == 0 {
 		return 0, nil
 	}
@@ -242,26 +211,26 @@ func (bl *GoLogger) Write(p []byte) (n int, err error) {
 		p = p[0 : len(p)-1]
 	}
 	// set levelLoggerImpl to ensure all log message will be write out
-	err = bl.writeMsg(levelLoggerImpl, string(p))
+	err = g.writeMsg(levelLoggerImpl, string(p))
 	if err == nil {
 		return len(p), err
 	}
 	return 0, err
 }
 
-func (bl *GoLogger) writeMsg(logLevel int, msg string, v ...interface{}) error {
-	if !bl.init {
-		bl.lock.Lock()
-		bl.setLogger(AdapterConsole)
-		bl.lock.Unlock()
+func (g *GoLogger) writeMsg(logLevel int, msg string, v ...interface{}) error {
+	if !g.init {
+		g.lock.Lock()
+		g.setLogger(AdapterConsole)
+		g.lock.Unlock()
 	}
 
 	if len(v) > 0 {
 		msg = fmt.Sprintf(msg, v...)
 	}
 	when := time.Now()
-	if bl.enableFuncCallDepth {
-		_, file, line, ok := runtime.Caller(bl.loggerFuncCallDepth)
+	if g.enableFuncCallDepth {
+		_, file, line, ok := runtime.Caller(g.loggerFuncCallDepth)
 		if !ok {
 			file = "???"
 			line = 0
@@ -278,14 +247,14 @@ func (bl *GoLogger) writeMsg(logLevel int, msg string, v ...interface{}) error {
 		msg = levelPrefix[logLevel] + msg
 	}
 
-	if bl.asynchronous {
+	if g.asynchronous {
 		lm := logMsgPool.Get().(*logMsg)
 		lm.level = logLevel
 		lm.msg = msg
 		lm.when = when
-		bl.msgChan <- lm
+		g.msgChan <- lm
 	} else {
-		bl.writeToLoggers(when, msg, logLevel)
+		g.writeToLoggers(when, msg, logLevel)
 	}
 	return nil
 }
@@ -293,45 +262,45 @@ func (bl *GoLogger) writeMsg(logLevel int, msg string, v ...interface{}) error {
 // SetLevel Set log message level.
 // If message level (such as LevelDebug) is higher than logger level (such as LevelWarning),
 // log providers will not even be sent the message.
-func (bl *GoLogger) SetLevel(l int) {
-	bl.level = l
+func (g *GoLogger) SetLevel(l int) {
+	g.level = l
 }
 
 // SetLogFuncCallDepth set log funcCallDepth
-func (bl *GoLogger) SetLogFuncCallDepth(d int) {
-	bl.loggerFuncCallDepth = d
+func (g *GoLogger) SetLogFuncCallDepth(d int) {
+	g.loggerFuncCallDepth = d
 }
 
 // GetLogFuncCallDepth return log funcCallDepth for wrapper
-func (bl *GoLogger) GetLogFuncCallDepth() int {
-	return bl.loggerFuncCallDepth
+func (g *GoLogger) GetLogFuncCallDepth() int {
+	return g.loggerFuncCallDepth
 }
 
 // EnableFuncCallDepth enable log funcCallDepth
-func (bl *GoLogger) EnableFuncCallDepth(b bool) {
-	bl.enableFuncCallDepth = b
+func (g *GoLogger) EnableFuncCallDepth(b bool) {
+	g.enableFuncCallDepth = b
 }
 
 // start logger chan reading.
 // when chan is not empty, write logs.
-func (bl *GoLogger) startLogger() {
+func (g *GoLogger) startLogger() {
 	gameOver := false
 	for {
 		select {
-		case bm := <-bl.msgChan:
-			bl.writeToLoggers(bm.when, bm.msg, bm.level)
+		case bm := <-g.msgChan:
+			g.writeToLoggers(bm.when, bm.msg, bm.level)
 			logMsgPool.Put(bm)
-		case sg := <-bl.signalChan:
-			// Now should only send "flush" or "close" to bl.signalChan
-			bl.flush()
+		case sg := <-g.signalChan:
+			// Now should only send "flush" or "close" to g.signalChan
+			g.flush()
 			if sg == "close" {
-				for _, l := range bl.outputs {
+				for _, l := range g.outputs {
 					l.Destroy()
 				}
-				bl.outputs = nil
+				g.outputs = nil
 				gameOver = true
 			}
-			bl.wg.Done()
+			g.wg.Done()
 		}
 		if gameOver {
 			break
@@ -340,145 +309,156 @@ func (bl *GoLogger) startLogger() {
 }
 
 // Emergency Log EMERGENCY level message.
-func (bl *GoLogger) Emergency(format string, v ...interface{}) {
-	if LevelEmergency > bl.level {
+func (g *GoLogger) Emergency(f interface{}, v ...interface{}) {
+	if LevelEmergency > g.level {
 		return
 	}
-	bl.writeMsg(LevelEmergency, format, v...)
+	format := formatLog(f, v...)
+	g.writeMsg(LevelEmergency, format, v...)
 }
 
 // Alert Log ALERT level message.
-func (bl *GoLogger) Alert(format string, v ...interface{}) {
-	if LevelAlert > bl.level {
+func (g *GoLogger) Alert(f interface{}, v ...interface{}) {
+	if LevelAlert > g.level {
 		return
 	}
-	bl.writeMsg(LevelAlert, format, v...)
+	format := formatLog(f, v...)
+	g.writeMsg(LevelAlert, format, v...)
 }
 
 // Critical Log CRITICAL level message.
-func (bl *GoLogger) Critical(format string, v ...interface{}) {
-	if LevelCritical > bl.level {
+func (g *GoLogger) Critical(f interface{}, v ...interface{}) {
+	if LevelCritical > g.level {
 		return
 	}
-	bl.writeMsg(LevelCritical, format, v...)
+	format := formatLog(f, v...)
+	g.writeMsg(LevelCritical, format, v...)
 }
 
 // Error Log ERROR level message.
-func (bl *GoLogger) Error(format string, v ...interface{}) {
-	if LevelError > bl.level {
+func (g *GoLogger) Error(f interface{}, v ...interface{}) {
+	if LevelError > g.level {
 		return
 	}
-	bl.writeMsg(LevelError, format, v...)
+	format := formatLog(f, v...)
+	g.writeMsg(LevelError, format, v...)
 }
 
 // Warning Log WARNING level message.
-func (bl *GoLogger) Warning(format string, v ...interface{}) {
-	if LevelWarn > bl.level {
+func (g *GoLogger) Warning(f interface{}, v ...interface{}) {
+	if LevelWarn > g.level {
 		return
 	}
-	bl.writeMsg(LevelWarn, format, v...)
+	format := formatLog(f, v...)
+	g.writeMsg(LevelWarn, format, v...)
 }
 
 // Notice Log NOTICE level message.
-func (bl *GoLogger) Notice(format string, v ...interface{}) {
-	if LevelNotice > bl.level {
+func (g *GoLogger) Notice(f interface{}, v ...interface{}) {
+	if LevelNotice > g.level {
 		return
 	}
-	bl.writeMsg(LevelNotice, format, v...)
+	format := formatLog(f, v...)
+	g.writeMsg(LevelNotice, format, v...)
 }
 
 // Informational Log INFORMATIONAL level message.
-func (bl *GoLogger) Informational(format string, v ...interface{}) {
-	if LevelInfo > bl.level {
+func (g *GoLogger) Informational(f interface{}, v ...interface{}) {
+	if LevelInfo > g.level {
 		return
 	}
-	bl.writeMsg(LevelInfo, format, v...)
+	format := formatLog(f, v...)
+	g.writeMsg(LevelInfo, format, v...)
 }
 
 // Debug Log DEBUG level message.
-func (bl *GoLogger) Debug(format string, v ...interface{}) {
-	if LevelDebug > bl.level {
+func (g *GoLogger) Debug(f interface{}, v ...interface{}) {
+	if LevelDebug > g.level {
 		return
 	}
-	bl.writeMsg(LevelDebug, format, v...)
+	format := formatLog(f, v...)
+	g.writeMsg(LevelDebug, format, v...)
 }
 
 // Warn Log WARN level message.
 // compatibility alias for Warning()
-func (bl *GoLogger) Warn(format string, v ...interface{}) {
-	if LevelWarn > bl.level {
+func (g *GoLogger) Warn(f interface{}, v ...interface{}) {
+	if LevelWarn > g.level {
 		return
 	}
-	bl.writeMsg(LevelWarn, format, v...)
+	format := formatLog(f, v...)
+	g.writeMsg(LevelWarn, format, v...)
 }
 
 // Info Log INFO level message.
 // compatibility alias for Informational()
-func (bl *GoLogger) Info(format string, v ...interface{}) {
-	if LevelInfo > bl.level {
+func (g *GoLogger) Info(f interface{}, v ...interface{}) {
+	if LevelInfo > g.level {
 		return
 	}
-	bl.writeMsg(LevelInfo, format, v...)
+	format := formatLog(f, v...)
+	g.writeMsg(LevelInfo, format, v...)
 }
 
 // Trace Log TRACE level message.
 // compatibility alias for Debug()
-func (bl *GoLogger) Trace(format string, v ...interface{}) {
-	if LevelDebug > bl.level {
+func (g *GoLogger) Trace(f interface{}, v ...interface{}) {
+	if LevelDebug > g.level {
 		return
 	}
-	bl.writeMsg(LevelDebug, format, v...)
+	format := formatLog(f, v...)
+	g.writeMsg(LevelDebug, format, v...)
 }
 
 // Flush flush all chan data.
-func (bl *GoLogger) Flush() {
-	if bl.asynchronous {
-		bl.signalChan <- "flush"
-		bl.wg.Wait()
-		bl.wg.Add(1)
+func (g *GoLogger) Flush() {
+	if g.asynchronous {
+		g.signalChan <- "flush"
+		g.wg.Wait()
+		g.wg.Add(1)
 		return
 	}
-	bl.flush()
+	g.flush()
 }
 
 // Close close logger, flush all chan data and destroy all adapters in GoLogger.
-func (bl *GoLogger) Close() {
-	if bl.asynchronous {
-		bl.signalChan <- "close"
-		bl.wg.Wait()
-		close(bl.msgChan)
+func (g *GoLogger) Close() {
+	if g.asynchronous {
+		g.signalChan <- "close"
+		g.wg.Wait()
+		close(g.msgChan)
 	} else {
-		bl.flush()
-		for _, l := range bl.outputs {
+		g.flush()
+		for _, l := range g.outputs {
 			l.Destroy()
 		}
-		bl.outputs = nil
+		g.outputs = nil
 	}
-	close(bl.signalChan)
+	close(g.signalChan)
 }
 
-// Reset close all outputs, and set bl.outputs to nil
-func (bl *GoLogger) Reset() {
-	bl.Flush()
-	for _, l := range bl.outputs {
+// Reset close all outputs, and set g.outputs to nil
+func (g *GoLogger) Reset() {
+	g.Flush()
+	for _, l := range g.outputs {
 		l.Destroy()
 	}
-	bl.outputs = nil
+	g.outputs = nil
 }
 
-func (bl *GoLogger) flush() {
-	if bl.asynchronous {
+func (g *GoLogger) flush() {
+	if g.asynchronous {
 		for {
-			if len(bl.msgChan) > 0 {
-				bm := <-bl.msgChan
-				bl.writeToLoggers(bm.when, bm.msg, bm.level)
+			if len(g.msgChan) > 0 {
+				bm := <-g.msgChan
+				g.writeToLoggers(bm.when, bm.msg, bm.level)
 				logMsgPool.Put(bm)
 				continue
 			}
 			break
 		}
 	}
-	for _, l := range bl.outputs {
+	for _, l := range g.outputs {
 		l.Flush()
 	}
 }
@@ -562,91 +542,58 @@ func SetLogger(adapter string, config ...string) error {
 
 // Emergency logs a message at emergency level.
 func Emergency(f interface{}, v ...interface{}) {
-	if LevelEmergency > goLogger.level {
-		return
-	}
-	goLogger.Emergency(formatLog(f, v...))
+	goLogger.Emergency(f, v...)
 }
 
 // Alert logs a message at alert level.
 func Alert(f interface{}, v ...interface{}) {
-	if LevelAlert > goLogger.level {
-		return
-	}
-	goLogger.Alert(formatLog(f, v...))
+	goLogger.Alert(f, v...)
 }
 
 // Critical logs a message at critical level.
 func Critical(f interface{}, v ...interface{}) {
-	if LevelCritical > goLogger.level {
-		return
-	}
-	goLogger.Critical(formatLog(f, v...))
+	goLogger.Critical(f, v...)
 }
 
 // Error logs a message at error level.
 func Error(f interface{}, v ...interface{}) {
-	if LevelError > goLogger.level {
-		return
-	}
-	goLogger.Error(formatLog(f, v...))
+	goLogger.Error(f, v...)
 }
 
 // Warning logs a message at warning level.
 func Warning(f interface{}, v ...interface{}) {
-	if LevelWarn > goLogger.level {
-		return
-	}
-	goLogger.Warn(formatLog(f, v...))
+	goLogger.Warn(f, v...)
 }
 
 // Warn compatibility alias for Warning()
 func Warn(f interface{}, v ...interface{}) {
-	if LevelWarn > goLogger.level {
-		return
-	}
-	goLogger.Warn(formatLog(f, v...))
+	goLogger.Warn(f, v...)
 }
 
 // Notice logs a message at notice level.
 func Notice(f interface{}, v ...interface{}) {
-	if LevelNotice > goLogger.level {
-		return
-	}
-	goLogger.Notice(formatLog(f, v...))
+	goLogger.Notice(f, v...)
 }
 
 // Informational logs a message at info level.
 func Informational(f interface{}, v ...interface{}) {
-	if LevelInformational > goLogger.level {
-		return
-	}
-	goLogger.Info(formatLog(f, v...))
+	goLogger.Info(f, v...)
 }
 
 // Info compatibility alias for Informational()
 func Info(f interface{}, v ...interface{}) {
-	if LevelInfo > goLogger.level {
-		return
-	}
-	goLogger.Info(formatLog(f, v...))
+	goLogger.Info(f, v...)
 }
 
 // Debug logs a message at debug level.
 func Debug(f interface{}, v ...interface{}) {
-	if LevelDebug > goLogger.level {
-		return
-	}
-	goLogger.Debug(formatLog(f, v...))
+	goLogger.Debug(f, v...)
 }
 
 // Trace logs a message at trace level.
 // compatibility alias for Warning()
 func Trace(f interface{}, v ...interface{}) {
-	if LevelTrace > goLogger.level {
-		return
-	}
-	goLogger.Trace(formatLog(f, v...))
+	goLogger.Trace(f, v...)
 }
 
 func formatLog(f interface{}, v ...interface{}) string {
@@ -658,9 +605,9 @@ func formatLog(f interface{}, v ...interface{}) string {
 			return msg
 		}
 		if strings.Contains(msg, "%") && !strings.Contains(msg, "%%") {
-			//format string
+			// format string
 		} else {
-			//do not contain format char
+			// do not contain format char
 			msg += strings.Repeat(" %v", len(v))
 		}
 	default:
@@ -670,5 +617,5 @@ func formatLog(f interface{}, v ...interface{}) string {
 		}
 		msg += strings.Repeat(" %v", len(v))
 	}
-	return fmt.Sprintf(msg, v...)
+	return msg
 }
